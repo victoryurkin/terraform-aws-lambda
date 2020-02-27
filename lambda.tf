e150a26ba4c074f7fc4eb69cf03dde5aad1ef56d
Victor Yurkin <victor.yurkin@aetion.com>
2020-02-27 16:50:49 -0500

    Updates

--- lambda.tf
+++ lambda.tf
@@ -2,7 +2,7 @@ resource "aws_cloudfront_origin_access_identity" "default" {
   comment = "main"
 }
 
-resource "aws_lambda" "default" {
+resource "aws_lambda_function" "default" {
   function_name = var.function_name
   
   tags = local.base_tags
