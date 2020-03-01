1d9da79176b955ba9af09b4c3ca6967815f454ba
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-01 12:09:30 -0500

    Updates

--- lambda.tf
+++ lambda.tf
@@ -2,17 +2,11 @@ resource "aws_cloudfront_origin_access_identity" "default" {
   comment = "main"
 }
 
-resource "aws_lambda_function" "default" {
-  function_name    = var.function_name
-  handler          = var.handler
-  runtime          = var.runtime
-  role             = var.role
-
-  data "archive_file" "lambda_zip_inline" {
-    type        = "zip"
-    output_path = "lambda_zip_inline.zip"
-    source {
-      content  = <<EOF
+data "archive_file" "lambda_zip_inline" {
+  type        = "zip"
+  output_path = "lambda_zip_inline.zip"
+  source {
+    content  = <<EOF
 exports.handler = async (event) => {
     // TODO implement
     const response = {
@@ -22,9 +16,15 @@ exports.handler = async (event) => {
     return response;
 };
 EOF
-      filename = "index.js"
-    }
-  }  
+    filename = "index.js"
+  }
+}  
+
+resource "aws_lambda_function" "default" {
+  function_name    = var.function_name
+  handler          = var.handler
+  runtime          = var.runtime
+  role             = var.role
 
   filename         = data.archive_file.lambda_zip_inline.output_path
   source_code_hash = data.archive_file.lambda_zip_inline.output_base64sha256
